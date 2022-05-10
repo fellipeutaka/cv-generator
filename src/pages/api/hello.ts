c8e29b7d22f5cc7de4a79bfa18d05514a14bf8f0
fellipeutaka <fellipeutaka@gmail.com>
2022-05-10 12:46:27 -0300

    * feat(Lib): PDFMake; * feat(hello api route): Hello World PDF;

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,32 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import PdfPrinter from "pdfmake";
+import { TDocumentDefinitions } from "pdfmake/interfaces";
 
-type Data = {
-  name: string
-}
+const fonts = {
+  Times: {
+    normal: "Times-Roman",
+    bold: "Times-Bold",
+    italics: "Times-Italic",
+    bolditalics: "Times-BoldItalic",
+  },
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+const printer = new PdfPrinter(fonts);
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const docDefinitions: TDocumentDefinitions = {
+    defaultStyle: { font: "Times" },
+    content: [{ text: "Hello World!" }],
+  };
+
+  const pdfDoc = printer.createPdfKitDocument(docDefinitions);
+  const chunks: Uint8Array[] = [];
+  pdfDoc.on("data", (chunk) => {
+    chunks.push(chunk);
+  });
+  pdfDoc.end();
+  pdfDoc.on("end", () => {
+    const result = Buffer.concat(chunks);
+    res.end(result);
+  });
 }
