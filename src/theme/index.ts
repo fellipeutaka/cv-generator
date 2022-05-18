194be9a7b70c0838aa05bd565f876006b2d14515
fellipeutaka <fellipeutaka@gmail.com>
2022-05-18 01:30:48 -0300

    theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,7 +2,7 @@ import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
   initialColorMode: "dark",
-  useSystemColorMode: true,
+  useSystemColorMode: false,
 };
 
 const theme = extendTheme({ config });
