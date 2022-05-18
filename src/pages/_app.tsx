936f4af4d44a577731668dd5165f95dc62a30608
fellipeutaka <fellipeutaka@gmail.com>
2022-05-18 00:30:17 -0300

    * fix(_app): theme;

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import theme from "../theme";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS>
+    <ChakraProvider theme={theme} resetCSS>
       <Component {...pageProps} />
     </ChakraProvider>
   );
